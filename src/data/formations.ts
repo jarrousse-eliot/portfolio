231d5741ea57b5ccf9b80b1776c96a78ef2b8044
jarrousse-eliot <etoil.jaleps@gmail.com>
2026-04-01 23:04:52 +0200

    json database (instead of included into type script)

--- src/data/formations.ts
+++ src/data/formations.ts
@@ -7,21 +7,6 @@ export type Formation = {
     skills: string[];
 };
 
-export const formations: Formation[] = [
-    {
-        degree: "formations.degrees.engineering",
-        school: "ESIEA",
-        year: "2022-2025",
-        logo: "https://www.esiea.fr/wp-content/uploads/2020/05/logo-esiea.png",
-        link: "https://www.esiea.fr",
-        skills: ["Cybersecurity", "AdvancedProgramming", "Agile", "MachineLearning"]
-    },
-    {
-        degree: "formations.degrees.preparatory",
-        school: "ESIEA",
-        year: "2020-2022",
-        logo: "https://www.esiea.fr/wp-content/uploads/2020/05/logo-esiea.png",
-        link: "https://www.esiea.fr",
-        skills: ["SoftwareEngineering", "WebDevelopment", "DatabaseManagement", "TeamCollaboration"]
-    }
-];
+import formationJson from "./formations.json";
+
+export const formations: Formation[] = formationJson as Formation[];
